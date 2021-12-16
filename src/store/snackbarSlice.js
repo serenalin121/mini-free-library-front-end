6ae1121366f8517ec9311992647916b5a2ea61da
serenalin121 <serenalin121@gmail.com>
2021-12-15 17:00:22 -0800

    added error snackbar message

--- src/store/snackbarSlice.js
+++ src/store/snackbarSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { addBook, deleteBook } from "./bookSlice";
 import { createLibrary, deleteLibrary } from "./librarySlice";
+import { signinUser } from "./userSlice";
+import { signinAdmin } from "./adminSlice";
 
 const initialState = {
   snackbarOpen: false,
@@ -11,6 +13,11 @@ const initialState = {
 const SHARED_SUCCESS_SNACKBAR = {
   snackbarOpen: true,
   snackbarType: "success",
+};
+
+const SHARED_ERROR_SNACKBAR = {
+  snackbarOpen: true,
+  snackbarType: "error",
 };
 
 const snackbarSlice = createSlice({
@@ -56,6 +63,22 @@ const snackbarSlice = createSlice({
         ...state,
         ...SHARED_SUCCESS_SNACKBAR,
         snackbarMessage: "Successfully deleted a library!",
+      };
+    });
+
+    builder.addCase(signinUser.rejected, (state, action) => {
+      return {
+        ...state,
+        ...SHARED_ERROR_SNACKBAR,
+        snackbarMessage: "Incorrect Password or Email Address...",
+      };
+    });
+
+    builder.addCase(signinAdmin.rejected, (state, action) => {
+      return {
+        ...state,
+        ...SHARED_ERROR_SNACKBAR,
+        snackbarMessage: "Incorrect Password or Email Address...",
       };
     });
   },
